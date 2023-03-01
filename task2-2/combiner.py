55ee88b8c55816fbbc24bf89774ba2036eab48be
Anton Lindholm <LinAnt@users.noreply.github.com>
2023-03-01 19:11:09 +0200

    Add combiner to task2-2

---
+++ task2-2/combiner.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+from validators import domain 
+
+domain_dict = {}
+
+for line in sys.stdin: 
+    # remove leading and trailing whitespace 
+    line = line.strip() 
+    # split the line into words 
+    words = line.split() 
+    d = words[0]
+    amount = int(words[1])
+    if domain(d): # Strip non-valid domains
+        if d in domain_dict:
+            count = domain_dict[d]
+            domain_dict[d] = count + amount
+        else:
+            domain_dict[d] = amount
+
+for d in domain_dict:
+    print('%s\t%s' % (d, domain_dict[d]))
--- task2-2/mapper.py
+++ task2-2/mapper.py
@@ -8,14 +8,7 @@ for line in sys.stdin:
     # remove leading and trailing whitespace 
     line = line.strip() 
     # split the line into words 
-    words = line.split() 
-    d = words[0] 
+    words = line.split()
+    d = words[0]
     if domain(d): # Strip non-valid domains
-        if d in domain_dict:
-            count = domain_dict[d]
-            domain_dict[d] = count + 1
-        else:
-            domain_dict[d] = 1
-
-for d in domain_dict:
-    print('%s\t%s' % (d, domain_dict[d]))
+        print('%s\t%s' % (d, 1))
